791b6ae13fa15a1f51815833fa72a3961037d9bd
NovruzShafiyev <qaqashshefiyev@gmail.com>
2024-03-18 04:23:09 +0400

    done

--- Javascript_advanced/1-nested_functions.js
+++ Javascript_advanced/1-nested_functions.js
@@ -1,3 +1,17 @@
-function globalVariable() {
-    
+
+let globalVariable = 'Welcome';
+function outer() {
+  alert(globalVariable);
+  let course = 'Holberton';
+  function inner() {
+    alert(globalVariable + ' ' + course);
+    let exclamation = '!';
+    function inception() {
+      alert(globalVariable + ' ' + course + exclamation);
+    }
+    inception();
+  }
+  inner();
 }
+outer();
+
